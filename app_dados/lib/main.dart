a2c0bdee324d3f24df5cb1514e8f30a0f191a79d
Nathalia Caetano <nathzcaetano@gmail.com>
2021-08-24 19:30:03 -0300

    Finish project Dados

---
+++ app_dados/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          title: Text('Dadoos'),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatefulWidget {
+  @override
+  _DadoosState createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+  int numeroDadoEsquerdo = 1;
+  int numeroDadoDireito = 1;
+
+  void _updateDados() {
+    setState(() {
+      numeroDadoEsquerdo = Random().nextInt(6) + 1;
+      numeroDadoDireito = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: _updateDados,
+              child: Image.asset('imagens/dado$numeroDadoEsquerdo.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: _updateDados,
+                child: Image.asset('imagens/dado$numeroDadoDireito.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
